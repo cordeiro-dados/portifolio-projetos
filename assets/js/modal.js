8dee7c038bce846fe343ad51423b6acb0a157e60
Gabriel Cordeiro <cordeirodados@gmail.com>
2024-10-29 09:57:13 -0300

    .

---
+++ assets/js/modal.js
@@ -0,0 +1,44 @@
+function openModalLogistica(event) {
+  // Previne o comportamento padrão (subir a página)
+  event.preventDefault();
+
+  // Abre o modal
+  document.getElementById("modal_logistica").style.display = "block";
+}
+
+
+
+function closeModalLogistica() {
+  document.getElementById("modal_logistica").style.display = "none";
+}
+
+
+function openModalVendas(event) {
+  // Previne o comportamento padrão (subir a página)
+  event.preventDefault();
+
+  // Abre o modal
+  document.getElementById("modal_vendas").style.display = "block";
+}
+
+
+
+function closeModalVendas() {
+  document.getElementById("modal_vendas").style.display = "none";
+}
+
+function openModalFluxo(event) {
+  // Previne o comportamento padrão (subir a página)
+  event.preventDefault();
+
+  // Abre o modal
+  document.getElementById("modal_fluxo").style.display = "block";
+}
+
+
+
+function closeModalFluxo() {
+  document.getElementById("modal_fluxo").style.display = "none";
+}
+
+  
